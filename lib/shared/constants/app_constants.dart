0a910ec5af3b74138d3db61fc12cddc37479f4a9
Ronak Prajapati <lucky.prajapati715@gmail.com>
2026-01-02 10:06:45 +0545

    fixing errors in app constants

--- lib/shared/constants/app_constants.dart
+++ lib/shared/constants/app_constants.dart
@@ -5,18 +5,4 @@ class AppConstants {
   static const String logoPath = 'assets/images/logo.png';
 }
 
-class AppStrings {
-  static const String welcome = 'Welcome to GearGhar';
-  static const String createAccount = 'Create Account';
-  static const String login = 'Login';
-  static const String signup = 'Sign Up';
-  static const String email = 'Email';
-  static const String password = 'Password';
-  static const String confirmPassword = 'Confirm Password';
-  static const String name = 'Name';
-  static const String forgotPassword = 'Forgot Password?';
-  static const String rememberMe = 'Remember Me';
-  static const String alreadyHaveAccount = 'Already have an account? Login';
-  static const String dontHaveAccount = "Don't have an account? Sign Up";
-  static const String agreeToTerms = 'I agree to the terms and conditions';
-}
+class AppStrings {}
